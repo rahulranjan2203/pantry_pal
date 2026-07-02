519a792c9ad5c85dc4b10c4e91da1827b2b51214
Rahul Ranjan <ranjanrahul2203>
2026-07-02 20:20:52 +0530

    Add database SSL support

--- backend/database.js
+++ backend/database.js
@@ -11,6 +11,11 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT || 3306,
     dialect: 'mysql',
     logging: false,
+    dialectOptions: process.env.DB_SSL === 'true' ? {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    } : {}
   }
 );
 
